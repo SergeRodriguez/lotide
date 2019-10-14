9651c7c9e36968c6c2d1bafbf616e12a8bee1445
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-14 20:52:12 +0000

    Adding assertEqual.js file

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('😀 😀 😀  Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log(' ☢ ☢ ☢ Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(25, 25);
