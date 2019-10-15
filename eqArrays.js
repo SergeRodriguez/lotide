77ca6d239661dfdcb402a7fe00e9e77dfbcf473e
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-15 21:54:56 +0000

    created eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,41 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😀 😀 😀  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫 🚫 🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true
+};
+
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false);
+assertEqual(eqArrays([1, 2, "Serge"], [1, 2, "serge"]), false);
+assertEqual(eqArrays(["1", "2", false], ["1", "2", NaN]), false);
+assertEqual(eqArrays([1, 2, false], [1, 2, undefined]), false);
+assertEqual(eqArrays([1, 2, false], [1, 2, 0]), false);
+assertEqual(eqArrays([1, 2, false], [1, 2, null]), false);
+assertEqual(eqArrays([1, 2, false], [1, 2, ""]), false);
+
+
+
+
+
+
