03fcfbb13a8ab2b4750500f16249b11a8f0235f3
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-15 22:44:38 +0000

    created assertArrayEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const assertArrayEqual = function(arr1, arr2) {
+
+  console.log(`The arrays are identical : ${eqArrays(arr1,arr2)}`);
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true
+};
+
+
+assertArrayEqual([1,2,3], [1,2,"3"]);
