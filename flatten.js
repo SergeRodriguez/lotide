1ee479a1043824e27553ff4300c6ae13d1a7348d
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-16 00:54:20 +0000

    created flatten.js

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`😀 😀 😀  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫 🚫 🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = function (arr) {
+  let flattenedArray = []
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        flattenedArray.push(arr[i][j])
+      }
+    } else {
+      flattenedArray.push(arr[i])
+    }
+  }
+  return flattenedArray
+}
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6])
+console.log(flatten([1, [2, [3, [4]], 5]])) // => [1, 2, [3, [4]], 5]
+
