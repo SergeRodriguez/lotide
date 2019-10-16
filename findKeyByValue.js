64df9a5600c4b33c45c7162b89ba7d264b6938bf
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-16 19:55:18 +0000

    created findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+//
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😀 😀 😀  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫 🚫 🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (object, value) {
+
+  for (let item in object) {
+    if (object[item] === value) {
+      return item
+    }
+  }
+}
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
