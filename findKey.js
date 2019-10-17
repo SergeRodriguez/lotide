e4cae968f373c1d95c6e9123b9f7fc7458c79ea3
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-17 19:26:54 +0000

    created findKey.js

---
+++ findKey.js
@@ -0,0 +1,42 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😀 😀 😀  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫 🚫 🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function (object, callback) {
+
+  for (key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+}
+
+
+
+const results1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2) // => "noma" */
+
+console.log(results1)
+assertEqual(results1, "noma")
+assertEqual(results1, "normand")
+
+var users = {
+  'barney': { 'age': 36, 'active': true },
+  'fred': { 'age': 40, 'active': false },
+  'pebbles': { 'age': 1, 'active': true }
+};
+
+const results2 = findKey(users, function (o) { return o.age < 40; });
+console.log(results2)
+assertEqual(results2, "barney")
+assertEqual(results2, "normand")
