f46034effd428921131beacea7d9ac449defb41b
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-21 21:05:29 +0000

    implemented the mocha framework and the chai library for testing in  middleTest.js and tailTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,23 @@
-const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
+const assert = require('chai').assert;
 
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+describe("#middle", () => {
+  it("returns [] when [1] is passed to middle()", () => {
+    assert.deepEqual(middle([1]), []);
+  })
+  it("returns [] when [1,2] is passed to middle()", () => {
+    assert.deepEqual(middle([1,2]), []);
+  })
+  it("returns [2] when [1,2] is passed to middle()", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  })
+  it("returns [2,4] when [1,2,3,4] is passed to middle()", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  })
+})
+
+
+
+//assert.deepEqual(middle([1, 2, 3, 4]), [2]);
+//assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,39 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual")
+const assert = require('chai').assert;
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
 
-// Test Case 2: ...
-const arr = ["Hello"];
-let result2 = tail(arr);
-assertEqual(arr.length, 1); // ensure we get back 3 elements
-assertEqual(arr[0], "Hello"); // 
-assertEqual(arr[1], undefined); // 
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns 2 when function is passed an array of length 3 like [1,2,3]", () => {
+    const inputName = [1, 2, 3]
+    const expectedOutput = 2
+    assert.deepEqual(tail(inputName).length, expectedOutput);
+  });
+
+  it("returns undfined for and empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+
+
+// const assertEqual = require("../assertEqual")
+
+// // Test Case 1: Check the returned array elements
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// // Test Case 2: ...
+// const arr = ["Hello"];
+// let result2 = tail(arr);
+// assertEqual(arr.length, 1); // ensure we get back 3 elements
+// assertEqual(arr[0], "Hello"); // 
+// assertEqual(arr[1], undefined); // 
