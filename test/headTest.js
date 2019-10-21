7d531505a8d8685b2b8b72bcacda37f4681f42fa
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-21 18:51:03 +0000

    implemented the mocha syntax for the headTest.js and the chai library for the assertions

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,24 @@
-const head = require("../head");
-const assertEqual = require("../assertEqual")
+const assert = require('chai').assert;
+const head = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns undfined for and empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+
+// const head = require("../head");
+// const assertEqual = require("../assertEqual")
 
 
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
