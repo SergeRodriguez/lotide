22c7e527c2f1847acdaee6b61c0b536b385cadc7
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-21 17:37:58 +0000

    created tailTest.js to test the tail method whic is supposed to return the tail of an array

---
+++ test/tailTest.js
@@ -0,0 +1,15 @@
+const tail = require("../tail");
+const assertEqual = require("../assertEqual")
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2: ...
+const arr = ["Hello"];
+let result2 = tail(arr);
+assertEqual(arr.length, 1); // ensure we get back 3 elements
+assertEqual(arr[0], "Hello"); // 
+assertEqual(arr[1], undefined); // 
